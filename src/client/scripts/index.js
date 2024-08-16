152c9e374eeae70291d61d2f2d2e03ce6a4a994c
theohaines <theohaines@yahoo.com>
2024-08-16 18:18:45 +0100

    create the public version of the contract calculator

---
+++ src/client/scripts/index.js
@@ -0,0 +1,87 @@
+//Sub menus
+const tradeinMenu = document.getElementById("tradeinMenu");
+const cashbackMenu = document.getElementById("cashbackMenu");
+
+//Select menus
+const tradeinSelected = document.getElementById("tradeinSelected");
+const cashbackType = document.getElementById("cashbackType");
+
+//Menus
+const breakdownScreen = document.getElementById("breakdown-screen");
+
+//Inputs
+const contractMonthlyCost = document.getElementById("contractMonthlyCost");
+const contractUpfrontCost = document.getElementById("contractUpfrontCost");
+const contractDuration = document.getElementById("contractDuration");
+
+const tradeinAmount = document.getElementById("tradeinAmount");
+const cashbackAmount = document.getElementById("cashbackAmount");
+const ccvAmount = document.getElementById("ccvAmount");
+
+//Outputs
+const youPayText = document.getElementById("youPayText");
+const costOfContractTotalAfterText = document.getElementById("costOfContractTotalAfterText");
+const costOfContractTotalText = document.getElementById("costOfContractTotalText");
+
+function hideAllMenus(){
+    tradeinMenu.style.display = "none";
+    cashbackMenu.style.display = "none";
+}
+
+function tradeinMenuChecker(){
+    if (tradeinSelected.options[tradeinSelected.selectedIndex].value == "none"){
+        tradeinMenu.style.display = "none";
+    } else if (tradeinSelected.options[tradeinSelected.selectedIndex].value == "tradein") {
+        tradeinMenu.style.display = "flex";
+    } else {
+        tradeinMenu.style.display == "none";
+    }
+
+    //console.log(tradeinSelected.options[tradeinSelected.selectedIndex].value)
+}
+
+function cashbackMenuChecker(){
+    if (cashbackType.options[cashbackType.selectedIndex].value == "none" || cashbackType.options[cashbackType.selectedIndex].value == "giftcard"){
+        cashbackMenu.style.display = "none";
+    } else if (cashbackType.options[cashbackType.selectedIndex].value == "bacs") {
+        cashbackMenu.style.display = "flex";
+    } else {
+        cashbackMenu.style.display == "none";
+    }
+
+    //console.log(cashbackType.options[cashbackType.selectedIndex].value)
+}
+
+async function calculateContract(){
+    let validated = await validateRequiredFields();
+
+    if (!validated){
+        alert("ERR: Not all fields are filled out!");
+    }
+
+    var costOfContractTotal = (contractMonthlyCost.value * contractDuration.value) + parseFloat(contractUpfrontCost.value);
+
+    youPayText.textContent = Math.round(costOfContractTotal * 100) / 100
+}
+
+async function validateRequiredFields(){
+    if (contractMonthlyCost <= 0 || contractUpfrontCost < 0){
+        return false;
+    }
+
+    if(tradeinSelected.options[tradeinSelected.selectedIndex].value == "tradein" && tradeinAmount.value <= 0){
+        return false;
+    }
+
+    if(cashbackType.options[cashbackType.selectedIndex].value == "bacs" && cashbackAmount.value <= 0){
+        return false;
+    }
+
+    return true;
+}
+
+function init(){
+    hideAllMenus();
+}
+
+init()
---
+++ src/server/contractcalc.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+//Resource routes
+
+app.use('/', express.static(path.resolve("src/client")));
+app.use('/styles', express.static(path.resolve("src/client/styles")));
+app.use('/scripts', express.static(path.resolve("src/client/scripts")));
+
+//Pages routes
+app.use('/landing', express.static(path.resolve("src/client/pages/landing")));
+
+//GET pages
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve("src/client/pages/landing/index.html"));
+});
+
+app.listen(process.env.PORT, () =>{
+    console.log("listening on: ", process.env.PORT);
+})
